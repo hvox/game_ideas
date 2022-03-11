7205b284ec2a4e6fdc56d6c4c3264c08a8b55ed0
Uladzislau Khamkou <kham@tuta.io>
2022-03-11 22:41:13 +0300

    implement one turn of battle

--- fightcalculator.py
+++ fightcalculator.py
@@ -6,3 +6,11 @@ class Squad:
     warriors: int
     health_per_unit: float
     damage_per_second: float
+
+    def __matmul__(self, other):
+        units1, h1, dps1 = dataclasses.astuple(self)
+        units2, h2, dps2 = dataclasses.astuple(other)
+        th1, th2 = h1 * units1, h2 * units2
+        th1 = max(0, th1 - units2 * dps2)
+        th2 = max(0, th2 - units1 * dps1)
+        return Squad(th1 / h1, h1, dps1), Squad(th2 / h2, h2, dps2)
