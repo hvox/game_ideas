423cb3c2933a7cfaaef85c5eef37389e6fbc289d
Uladzislau Khamkou <kham@tuta.io>
2023-01-08 23:56:52 +0300

    Switch to better tile encoding

---
+++ factorio-things/factorio/binary_encoding.py
@@ -0,0 +1,99 @@
+ENCODING = {
+    (): "0",
+    ("CUSTOM",): "1"
+}
+
+assert len(set(ENCODING.values())) == len(ENCODING)
+
+
+ENTITY = tuple[str, dict[str, str | int]]
+ENTITIES = dict[tuple[float, float], ENTITY]
+
+
+def str_to_bin(string: str) -> str:
+    result = [bin(len(string))[2:].zfill(8)]
+    for char in string:
+        result.append(bin(ord(char))[2:].zfill(8))
+    return "".join(result)
+
+
+def bin_to_str(binary: str, i: int = 0) -> tuple[str, int]:
+    length = int(binary[i: i + 8], 2)
+    result = []
+    for _ in range(length):
+        i += 8
+        result.append(chr(int(binary[i: i + 8], 2)))
+    return "".join(result), i + 8
+
+
+def encode_entity(entity: None | ENTITY) -> str:
+    if entity is None:
+        return ENCODING[()]
+    attributes = entity[1].copy()
+    direction = attributes.pop("direction")
+    key = (entity[0], direction) + sum(sorted(attributes.items()), ())
+    if key in ENCODING:
+        return ENCODING[key]
+    result = str_to_bin(entity[0]) + bin(direction)[2:].zfill(3)
+    result += bin(len(key) // 2 - 1)[2:].zfill(8)
+    for i in range(2, len(key), 2):
+        result += str_to_bin(key[i])
+        result += str_to_bin(key[i+1])
+    return ENCODING[("CUSTOM",)] + result
+
+
+def decode_entity(binary: str, i: int = 0) -> tuple[ENTITY | None, int]:
+    for key, code in ENCODING.items():
+        if binary[i: i + len(code)] == code:
+            break
+    if not key:
+        return None, i + len(code)
+    if key != ("CUSTOM",):
+        attributes = {"direction": key[1]}
+        for j in range(2, len(key), 2):
+            attributes[key[j]] = key[j + 1]
+        return (key[0], attributes), i + len(code)
+    name, i = bin_to_str(binary, i + len(code))
+    direction = int(binary[i:i+3], 2)
+    attrs_count = int(binary[i+3:i+11], 2)
+    i, attributes = i + 11, {"direction": direction}
+    for _ in range(attrs_count):
+        attr_name, i = bin_to_str(binary, i)
+        attr_value, i = bin_to_str(binary, i)
+        attributes[attr_name] = attr_value
+    return (name, attributes), i
+
+
+def encode_tile(width: int, height: int, entities: ENTITIES) -> str:
+    assert width < 64 and height < 64
+    grid = [None] * width * height
+    for pos, entity in entities.items():
+        x, y = (int(x) if x.is_integer() else int(x - 0.5) for x in pos)
+        assert 0 <= x < width and 0 <= y < height
+        grid[y * width + x] = entity
+    result = [bin(width)[2:].zfill(6), bin(height)[2:].zfill(6)]
+    for entity in grid:
+        result.append(encode_entity(entity))
+    return "".join(result)
+
+
+def decode_tile(code: str) -> tuple[tuple[int, int], ENTITIES]:
+    width, height = int(code[:6], 2), int(code[6:12], 2)
+    entities, i = {}, 12
+    for pos in range(width * height):
+        int_y, int_x = divmod(pos, width)
+        entity, i = decode_entity(code, i)
+        if entity is None:
+            continue
+        name, attrs = entity
+        if name == "splitter":
+            if attrs["direction"] % 4 == 2:
+                # splitter points to right or left
+                x, y = int_x + 0.5, float(int_y)
+            else:
+                # splitter points up or down
+                x, y = float(int_x), int_y + 0.5
+        else:
+            x, y = int_x + 0.5, int_y + 0.5
+        entities[x, y] = entity
+    return (width, height), entities
--- factorio-things/factorio/blueprints.py
+++ factorio-things/factorio/blueprints.py
@@ -2,12 +2,12 @@ import zlib
 import base64
 import json
 import math
+from . import binary_encoding
 from dataclasses import dataclass
 from typing import Self
 
 
 DIRECTED_STRUCTURES = {"transport-belt", "splitter", "express-loader", "underground-belt"}
-ENTITIES = ["transport-belt", "splitter", "underground-belt"]
 
 
 def as_int(x: float) -> int:
@@ -61,8 +61,10 @@ class Blueprint:
         for attrs in json_obj["entities"]:
             name, _ = attrs.pop("name"), attrs.pop("entity_number")
             position = attrs.pop("position")
-            x, y = position["x"], size[1]-position["y"]
+            x, y = float(position["x"]), float(size[1]-position["y"])
             attrs["direction"] = attrs.get("direction", 0)
+            if "neighbours" in attrs:
+                del attrs["neighbours"]
             entities[x, y] = (name, dict(sorted(attrs.items())))
         entities = dict(sorted(entities.items(), key=lambda x: (x[0][1], x[0][0])))
         return cls(size, entities)
@@ -81,30 +83,24 @@ class Blueprint:
         compressed = zlib.compress(json_str.encode(), 9)
         return "0" + base64.b64encode(compressed).decode()
 
-    def encode_tile(self) -> str:
-        width, height = self.size
-        entities = bytearray([0] * width * height)
-        for (x, y), (name, attrs) in self.entities.items():
-            pos = width * as_int(y - 0.5) + as_int(x - 0.5)
-            assert 0 <= pos < width * height and entities[pos] == 0
-            entity_id = 1 + ENTITIES.index(name)
-            entities[pos] = entity_id * 4 + attrs["direction"] // 2
-        code = base64.b85encode(entities).decode()
-        lines = (len(code) + 119) // 120
-        line_length = (len(code) + lines - 1) // lines
-        code = "\n".join([code[i:i + line_length] for i in range(0, len(code), line_length)])
-        return code
+    def __int__(self) -> int:
+        binary = binary_encoding.encode_tile(*self.size, self.entities)
+        return int("1" + binary, 2)
 
     @classmethod
-    def decode_tile(cls, width: int, height: int, encoded: str) -> Self:
-        entities = {}
-        encoded_entities = base64.b85decode(encoded.replace("\n", ""))
-        for pos, code in enumerate(encoded_entities):
-            x, y = pos % width + 0.5, pos // width + 0.5
-            name = ENTITIES[code // 4 - 1]
-            attrs = {"direction": code % 4 * 2}
-            entities[x, y] = (name, attrs)
-        return cls((width, height), entities)
+    def from_int(cls, index: int) -> Self:
+        return cls(*binary_encoding.decode_tile(bin(index)[3:]))
+
+    def to_tile_code(self) -> str:
+        number = int(self)
+        bts = number.to_bytes((number.bit_length() + 7) // 8, "little")
+        return base64.b85encode(bts).decode()
+
+    @classmethod
+    def from_tile_code(cls, code: str) -> Self:
+        bts = base64.b85decode(code)
+        number = int.from_bytes(bts, "little")
+        return cls.from_int(number)
 
     def rotated(self, quarters: int) -> Self:
         origin_x, origin_y = (x / 2 for x in self.size)
@@ -147,6 +143,7 @@ class BlueprintGrid:
 
 
 def decode(encoded: str) -> dict[tuple[float, float], tuple[str, dict[str, str]]]:
+    __import__("warnings").warn("factorio.blueprints.decode is depricated")
     assert encoded[0] == "0"
     compressed = base64.b64decode(encoded[1:])
     decoded = json.loads(zlib.decompress(compressed).decode())
@@ -165,6 +162,7 @@ def decode(encoded: str) -> dict[tuple[float, float], tuple[str, dict[str, str]]
 
 
 def encode(entities: dict[tuple[float, float], tuple[str, dict[str, str]]]) -> str:
+    __import__("warnings").warn("factorio.blueprints.encode is depricated")
     entities_json = []
     for i, ((x, y), (name, attrs)) in enumerate(entities.items()):
         entity_json = dict(attrs)
@@ -183,6 +181,7 @@ def rotated(
     origin_x: float, origin_y: float,
     entities: dict[tuple[float, float], tuple[str, dict[str, str]]],
 ) -> dict[tuple[float, float], tuple[str, dict[str, str]]]:
+    __import__("warnings").warn("factorio.blueprints.rotated is depricated")
     rotated_entities = {}
     for (x, y), (name, entity) in entities.items():
         dx, dy = x - origin_x, y - origin_y
@@ -191,15 +190,3 @@ def rotated(
             rotated_entity["direction"] = (rotated_entity["direction"] - 2) % 8
         rotated_entities[origin_x - dy, origin_y + dx] = (name, rotated_entity)
     return rotated_entities
-
-
-if __name__ == "__main__":
-    import clipboard
-    sample = "0eNrNnNtO20oUht/F10nlOY95la0KBfCmlhInip22qOLdt0PaxoB/Zv2Lm31RVRDy+Z+ZdZqTf1V321N7OHb9WN38qrr7fT9UN//8qobusd9sz78bnw5tdVN1Y7urVlW/2Z1/2gxDu7vbdv3jere5/9b17dpWz6uq6x/an9WNeV4VEeNx0w+H/XFc37XbcfZl+/x1VbX92I1dexHz8sPTbX/a3bXHiY4Yq+qwH6av7fvzUyfU2oT0Jayqp+rGWfMlPJ91vaFZguZtieb+0oZxP/XKj812u6ir+UtyyyQvJeUSKUhJsUSKup4HtKSj2WValrbSl3Q1UlIokUwtRdkiykhRroiyUlRdRInt3RRRUoP3RYM3Uov3RS80UYoqOo9JUlTRc4zU2H3R2I3U2n3R2m2tC6UIZ3Q4EB+s1Op90YGs1Op90YGs2OqLDmQDE0+vTjSZyJR8H7pje3/5E7sEjzp4AFoTnyc9QGUehVQ1uowElLn643rp45QSXoZlGpTusFguvX/c1V/+3QzjuuuH9jhOn3ycJIJk+J3lm2JQU7rjvl8/tpvj+se3tt0utsXRz5ulhcg/z/PP8+h5gqEKutCGDI1xTm9KbuCSDvcukMQleOZjJ2p2w6NAk32tC28R4IwOlwDO8iEOKVNMUJAqT8Sb9MZZCvHGU7ksvpL6Cu6X4JEQ7kk2NakJGL7kOz4Twg0pvJGzZ7VulOgONcG2JNvoghXwj2B1OOAjQVEpImWeRyFVgQ8CGaAij2oAKumCJsJlHQ41VFkbZokRx1oX7mRww9dufx/hara2iVYVXRtJWoiOYAfcTUsBMHpmDDw5BjOPO2y7sVyWNwIoNSezpOJEdLUtOWOknNGQUinXrEtaU63DibQmYpLmM2fBicpdmXO95Bh4Q8IZ35tVHzI4NdlKJJyaepHhLhE+6D1pLIxHztb+ZMIb3fxWBM+1rkQDiT0rC0gQPbLlyzSkTFE8IlWeLtMc2OHKfPH4UkQsoXSLilCZroCE6rKmkPldMJVsuOFrMvuq/dz6VqPLaahrmqvPnPqH9vh43E//S4AX6b83hPen8XBaLCIbfr3xGrmd5fvHUQ3yDd0gr0qfcAACkRlSkRY1eQbSEj92Do1deb7RZM3iwns3XUqSTaPKDahnTK1LXSjgmdrQGQJr4xMX1sWvMjq4ha5IXRaxdFtkWJsygUF96TNTdc+6jqnzZ9KQp8OsqWdL9qe7Ydy8sBdTxp+uXz4MwawohjfBpTBFM0ZXEcJRNbo1RWh1hq8LsTZ+VRHrUlSGAbH4dUXnEUtZF0JtupVFrK9RFZqifXtjlZUfarxVl35+Xil1PSiUjP1U6UfvmhurLv2kDVKWfnAEgmrJQWovURVZRXu/xiYVPMiU58/UnfyZAWN1qxzCgXBUrehYujLHoRjmlDkOGblT5DioTZHjoC5FjkuIpchxEbGUOQ5qU+Y4qE+3feZE++LG15qjCFAsd3wjsGKtaqNJSldtkbkomQ4bT+2ROVZ6UO1nSelRs6El7Zik2t+SSs+6Gg45t9dtmEF/CbWuwoE8o+Oh9garWi0TnZsx8yMeH2ztXnetXBbk5+BVlZLMnkLQJWs4XkzNaNj+TXz2hkL5w4fYqBo+ezfokkHNszJifea4h6ePexjqvEd6Jb88+LoDH7hv/CfWu972jWSKEIMq6UD9URe4od0l2bEUU9KVVScP3hvBYiykznf4sofMDnh8GLGbQu8lozpYIGx1UqyDxM/YKnfCI7DN8ao9JtGhGjM74iHuK/vKO7iYlyKfR6A1KvIbtEj+LoxHeziJz28e7S3lWrPC6UVb6YY6wHENilis1fFQR2anOfL1vvGLlp91i4tYbNDxYGeqtpaljeddBwvlS0PciQ19d9ujDZqGLw0xy9A3wTFLfCcllVmOvruNWZ6+vY1Zgb6+jVmRvr+NWYm+dY1ZV7tvfx6O7TCst/vNA4jNV9t3klMvpmnoS91IqZ0dnigrnYUmkVIrP0oRy0otfdMbsxx91RuzPH3XG7PEd+xtmRXpq9mYpcgJkJXpy9mYdfWErv+366cP1/ff2qFUVVy2c/585XZox3GqbYfznx7b3f57e3uaPttOKbV9uD2/YWX6aDye2lV1+e3lDSp/nnyeBhy2m7Gdnnq/P53f+vJyKGm3f7j41eZ+3D5Vsxe5fF2+ml8TrZn54v+gNV9XlxfR3MxefbOqvk/sS4mZJ69sbIph+mfS8/N/VGOhlA=="
-    blueprint = decode(sample)
-    print(blueprint)
-    print({name for name, _ in blueprint.values()})
-    rotated_blueprint = rotated(0, 0, blueprint)
-    encoded = encode(rotated_blueprint)
-    print(encoded)
-    clipboard.copy(encoded)
