76c6761b46340615c15cf282762d0bffaaa32182
Uladzislau Khamkou <kham@tuta.io>
2023-01-09 20:39:40 +0300

    Fix typos

--- factorio-things/factorio/recipes.py
+++ factorio-things/factorio/recipes.py
@@ -5,23 +5,23 @@ from typing import NamedTuple
 
 class Recipe(NamedTuple):
     creation_time: Fraction
-    ingridients: dict[str, Fraction]
+    ingredients: dict[str, Fraction]
 
     def __str__(self) -> str:
         # TODO: implement better string representation for amounts
-        ingridients = (f"{ing}:{x}" for ing, x in self.ingridients.items())
-        return f"time:{self.creation_time} + " + " + ".join(ingridients)
+        ingredients = (f"{ing}:{x}" for ing, x in self.ingredients.items())
+        return f"time:{self.creation_time} + " + " + ".join(ingredients)
 
 
 def read_recipes(item_group: str) -> dict[str, Recipe]:
     recipes = {}
     path = Path(__file__).with_suffix("") / (item_group + ".tsv")
     for row in path.read_text().strip("\n").split("\n")[1:]:
-        ingridients = {}
+        ingredients = {}
         material, creation_time, rest = row.split("\t")
         for ingridient, amount in (ing.split(":") for ing in rest.split(", ")):
-            ingridients[ingridient] = Fraction(amount)
-        recipes[material] = Recipe(Fraction(creation_time), ingridients)
+            ingredients[ingridient] = Fraction(amount)
+        recipes[material] = Recipe(Fraction(creation_time), ingredients)
     return recipes
 
 
