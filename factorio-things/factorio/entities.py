ce2ff015ef19af8e43dfa7e0cc000afee9b43f34
Uladzislau Khamkou <kham@tuta.io>
2023-01-22 20:06:30 +0300

    Use better names for types

--- factorio-things/factorio/entities.py
+++ factorio-things/factorio/entities.py
@@ -1,6 +1,6 @@
-ENTITY_ATTRIBUTES = dict[str, object]
-ENTITY = tuple[str, ENTITY_ATTRIBUTES]
-ENTITIES = dict[tuple[float, float], ENTITY]
+EntityAttributes = dict[str, object]
+Entity = tuple[str, EntityAttributes]
+Entities = dict[tuple[float, float], Entity]
 
 BELTS = ["transport-belt", "fast-transport-belt", "express-transport-belt"]
 UNDERGROUND_BELTS = ["underground-belt", "fast-underground-belt", "express-underground-belt"]
@@ -8,13 +8,13 @@ SPLITTERS = ["splitter", "fast-splitter", "express-splitter"]
 ASSEMBLY_MACHINES = ["assembly-machine-1", "assembly-machine-2", "assembly-machine-3"]
 
 
-def belt(lvl: int, direction: int) -> ENTITY:
+def belt(lvl: int, direction: int) -> Entity:
     assert 1 <= lvl <= 3
     name = BELTS[lvl - 1]
     return (name, {"direction": direction % 8 // 2 * 2})
 
 
-def underground(lvl: int, direction: int) -> tuple[ENTITY, ENTITY]:
+def underground(lvl: int, direction: int) -> tuple[Entity, Entity]:
     assert 1 <= lvl <= 3
     name = UNDERGROUND_BELTS[lvl - 1]
     inp = {"direction": direction % 8 // 2 * 2, "type": "input"}
@@ -28,10 +28,10 @@ def splitter(
     input_prioriy: int = 0,
     output_priority: int = 0,
     filter_material: str = "",
-) -> ENTITY:
+) -> Entity:
     assert 1 <= lvl <= 3
     name = SPLITTERS[lvl - 1]
-    attributes: ENTITY_ATTRIBUTES = {"direction": direction % 8 // 2 * 2}
+    attributes: EntityAttributes = {"direction": direction % 8 // 2 * 2}
     if input_prioriy:
         attributes["input_prioriy"] = ["", "right", "left"][input_prioriy]
     if output_priority:
@@ -41,10 +41,10 @@ def splitter(
     return (name, attributes)
 
 
-def assembler(lvl: int, produced_material: str = "", direction: int = 0) -> ENTITY:
+def assembler(lvl: int, produced_material: str = "", direction: int = 0) -> Entity:
     assert 1 <= lvl <= 3
     name = ASSEMBLY_MACHINES[lvl - 1]
-    attributes: ENTITY_ATTRIBUTES = {"direction": direction % 8 // 2 * 2}
+    attributes: EntityAttributes = {"direction": direction % 8 // 2 * 2}
     if produced_material:
         attributes["recipe"] = produced_material
     return (name, attributes)
