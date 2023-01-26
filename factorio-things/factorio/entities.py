863a156360c260e1773fd1f2b1eab064b384da40
Uladzislau Khamkou <kham@tuta.io>
2023-01-26 17:30:49 +0300

    Add printer of entities as python code

--- factorio-things/factorio/entities.py
+++ factorio-things/factorio/entities.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Any
 
 EntityAttributes = dict[str, object]
 Entity = tuple[str, EntityAttributes]
@@ -7,12 +8,21 @@ Entities = dict[tuple[float, float], Entity]
 BELTS = ["transport-belt", "fast-transport-belt", "express-transport-belt"]
 UNDERGROUND_BELTS = ["underground-belt", "fast-underground-belt", "express-underground-belt"]
 SPLITTERS = ["splitter", "fast-splitter", "express-splitter"]
-ASSEMBLY_MACHINES = ["assembly-machine-1", "assembly-machine-2", "assembly-machine-3"]
+ASSEMBLY_MACHINES = ["assembling-machine-1", "assembling-machine-2", "assembling-machine-3"]
+INSERTERS = ["inserter"]
+ELECTRIC_POLES = ["small-electric-pole", "medium-electric-pole", "big-electric-pole", "substation"]
 
 
 def belt(lvl: int, direction: int) -> Entity:
     assert 1 <= lvl <= 3
     name = BELTS[lvl - 1]
+    assert direction % 2 == 0
+    return (name, {"direction": direction % 8 // 2 * 2})
+
+
+def inserter(lvl: int, direction: int) -> Entity:
+    assert 1 <= lvl <= 1
+    name = INSERTERS[lvl - 1]
     assert direction % 2 == 0
     return (name, {"direction": direction % 8 // 2 * 2})
 
@@ -24,6 +34,12 @@ def underground(lvl: int, direction: int) -> tuple[Entity, Entity]:
     inp = {"direction": direction % 8 // 2 * 2, "type": "input"}
     out = {"direction": direction % 8 // 2 * 2, "type": "output"}
     return ((name, inp), (name, out))
+
+
+def electric_pole(lvl: int) -> Entity:
+    assert 1 <= lvl <= 4
+    name = ELECTRIC_POLES[lvl - 1]
+    return (name, {"direction": 0})
 
 
 def splitter(
@@ -64,3 +80,41 @@ def read_entities(table_name: str) -> dict[str, tuple[int, int]]:
 
 
 ENTITIES: dict[str, tuple[int, int]] = read_entities("entities")
+
+
+def entity_to_python(entity: Entity) -> str:
+    attrs: Any
+    name, attrs = entity
+    if name in BELTS:
+        lvl = BELTS.index(name) + 1
+        direction = (int(attrs["direction"]) + 2) % 8 - 2
+        return f"belt({lvl}, {direction})"
+    elif name in UNDERGROUND_BELTS:
+        ...  # TODO: support underground belts
+    elif name in INSERTERS:
+        lvl = INSERTERS.index(name) + 1
+        direction = (attrs["direction"] + 2) % 8 - 2
+        return f"inserter({lvl}, {direction})"
+    elif name in SPLITTERS:
+        lvl = SPLITTERS.index(name) + 1
+        direction = (attrs["direction"] + 2) % 8 - 2
+        input_priority = ["left", "", "right"].index(attrs.get("input_priority", "")) - 1
+        output_priority = ["left", "", "right"].index(attrs.get("output_priority", "")) - 1
+        filter = attrs.get("filter", "")
+        return f"splitter({lvl}, {direction}, {input_priority}, {output_priority}, {filter!r})"
+    elif name in ASSEMBLY_MACHINES:
+        lvl = ASSEMBLY_MACHINES.index(name) + 1
+        direction = (attrs["direction"] + 2) % 8 - 2
+        product = attrs.get("recipe", "")
+        return f"assembler({lvl}, {product!r}, {direction})"
+    elif name in ELECTRIC_POLES:
+        lvl = ELECTRIC_POLES.index(name) + 1
+        return f"electric_pole({lvl})"
+    return repr(entity)
+
+
+def entities_to_python(entities: Entities) -> list[str]:
+    code = ["x, y = 0, 0  # bottom left corner", "entities: Entities = {}"]
+    for (x, y), entity in sorted(entities.items()):
+        code.append(f"entities[x + {x}, y + {y}] = {entity_to_python(entity)}")
+    return code
