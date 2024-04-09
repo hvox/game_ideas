bc57a78a3c23de6eff909906a2dc982ae0bd492a
Uladzislau Khamkou <kham@tuta.io>
2024-04-09 23:09:56 +0300

    Add asymmetry checking

--- experiments/searching-for-tileset-palette-7x7.src/src/main.rs
+++ experiments/searching-for-tileset-palette-7x7.src/src/main.rs
@@ -39,6 +39,28 @@ fn print_grid(grid: &[u8]) {
 	}
 }
 
+fn asymmetry_score(grid: &[u8]) -> i32 {
+	fn bit(mask: u8, index: u8) -> i32 {
+		((mask >> (index - 1)) & 1).into()
+	}
+	let mut delta: i32 = 0;
+	for y in 0..7 {
+		for x in 0..y {
+			let t1 = grid[x + y * 7];
+			let t2 = grid[y + x * 7];
+			delta += (bit(t1, 1) + bit(t2, 1)) % 2;
+			delta += (bit(t1, 2) + bit(t2, 4)) % 2;
+			delta += (bit(t1, 3) + bit(t2, 6)) % 2;
+			delta += (bit(t1, 4) + bit(t2, 2)) % 2;
+			delta += (bit(t1, 5) + bit(t2, 7)) % 2;
+			delta += (bit(t1, 6) + bit(t2, 3)) % 2;
+			delta += (bit(t1, 7) + bit(t2, 5)) % 2;
+			delta += (bit(t1, 8) + bit(t2, 8)) % 2;
+		}
+	}
+	delta
+}
+
 #[allow(dead_code)]
 fn is_grid_tilable(grid: &[u8]) -> bool {
 	for y in 0..4 {
@@ -57,20 +79,38 @@ fn is_grid_tilable(grid: &[u8]) -> bool {
 }
 
 static mut GRIDS_FOUND: i32 = 0;
+static mut MIN_SCORE: i32 = 2147483647;
 fn search() -> i32 {
 	fn dfs(i: usize, grid: &mut [u8; 49], tiles: &mut [u8; 49]) -> i32 {
 		let x = i % 7;
 		let y = i / 7;
 		if y > 6 {
+			let score = asymmetry_score(grid);
+			if score > 2147483646 {
+				return 0;
+			};
 			unsafe {
 				GRIDS_FOUND += 1;
-				println!("#{}  :  {:?}", GRIDS_FOUND, grid);
+				MIN_SCORE = MIN_SCORE.min(score);
+				println!("\n#{} : score={} best={}\n{:?}", GRIDS_FOUND, score, MIN_SCORE, grid);
 			}
 			print_grid(grid);
 			// unsafe { if GRIDS_FOUND == 1 { panic!("!") }; }
 			return 1;
 		}
 		if grid[i] != NONE_TILE {
+			let tile = grid[i];
+			let left = if x > 0 { grid[x - 1 + y * 7] } else { 0 };
+			if bit(tile, 1) != bit(left, 3)
+				|| bit(tile, 4) != bit(left, 5)
+				|| bit(tile, 6) != bit(left, 8)
+			{
+				return 0;
+			}
+			let down = if y > 0 { grid[x + (y - 1) * 7] } else { 0 };
+			if tile & 0b111 != (down >> 5) & 0b111 {
+				return 0;
+			}
 			return dfs(i + 1, grid, tiles);
 		}
 		let mut result = 0;
@@ -101,7 +141,7 @@ fn search() -> i32 {
 
 	let mut grid = [NONE_TILE; 49];
 	let mut tiles: [u8; 49] = TILES.to_vec().try_into().unwrap();
-	[(6, 0), (42, 0), (48, 0)].into_iter().for_each(|(i, tile)| {
+	[(0, 0), (6, 0), (6 * 7, 0)].into_iter().for_each(|(i, tile)| {
 		grid[i] = tile;
 		tiles = tiles
 			.into_iter()
